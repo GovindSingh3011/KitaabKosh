580ab8db0394a39f2e94687f2a3c9463b86f70b4
Govind Singh <110704907+GovindSingh3011@users.noreply.github.com>
2025-01-27 00:04:30 +0530

    Add About route to main application

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import Home from "./routes/home.jsx";
 import Dashboard from "./routes/dashboard.jsx";
 import { Toaster } from "./components/ui/sonner";
+import About from "./routes/About";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/about" element={<About />} />
     </Routes>
     <Toaster richColors />
   </BrowserRouter>,
