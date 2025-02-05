6837c8b92e7b2976e0dd0550d1c319d225d079f0
Aman Varshney <amanvarshney.work@gmail.com>
2025-02-05 13:40:52 +0530

    code cleanup

--- server/src/routes/healthcheck.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { healthcheck } from "../controllers/healthcheck.controller.js";
-import { upload } from "../middlewares/multer.middlewares.js";
-
-const router = Router();
-
-router.route("/").get(upload.single('avatarnpm i cloudinary'),healthcheck);
-
-export default router;
