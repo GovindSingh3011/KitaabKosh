17fa403323ed8c6db2cf09e80450d2a3f1fd4a36
Govind Singh <110704907+GovindSingh3011@users.noreply.github.com>
2025-02-10 12:57:30 +0530

    replace Loader component in PrivateRoute

--- client/src/components/private-route.jsx
+++ client/src/components/private-route.jsx
@@ -2,12 +2,17 @@
 
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../components/auth-context";
+import Loader from "../components/ui/Loader";
 
 export default function PrivateRoute({ children }) {
   const { user, loading } = useAuth();
 
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <Loader />
+      </div>
+    );
   }
 
   if (!user) {
