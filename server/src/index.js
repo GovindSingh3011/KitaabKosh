544f650ff50d95650926d63eb45ff984d0f962de
GovindSingh3011 <govindrawat3011@gmail.com>
2025-04-29 23:20:26 +0530

    implement dynamic CORS origin handling for better security

--- server/src/index.js
+++ server/src/index.js
@@ -95,9 +95,19 @@ app.use(
   }),
 );
 
+const cors = require('cors');
+
+const allowedOrigins = process.env.CLIENT_URL; 
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin:function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
